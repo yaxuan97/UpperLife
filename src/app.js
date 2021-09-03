826e0e1e5be313bc0f196e5bd56cff085c90a67d
nbqq <90015236+nbqq@users.noreply.github.com>
2021-09-03 02:51:36 -0600

    rebrand

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ class App{
         const loadingPage = $(`
         <div id="main">
             <div id="title">
-                人上人重开模拟器<br>
+                人生重开模拟器-人上人版<br>
                 <div style="font-size:1.5rem; font-weight:normal;">加载中...</div>
             </div>
         </div>
@@ -42,7 +42,7 @@ class App{
             <div id="cnt" class="head">已重开1次</div>
             <button id="rank">排行榜</button>
             <div id="title">
-                人上人重开模拟器<br>
+                人生重开模拟器-人上人版<br>
                 <div style="font-size:1.5rem; font-weight:normal;">这垃圾人生一秒也不想呆了</div>
             </div>
             <button id="restart" class="mainbtn"><span class="iconfont">&#xe6a7;</span>立即重开</button>
