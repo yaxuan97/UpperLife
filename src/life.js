9ae149ca8141c2d1c63c34a19df4cf83a282c4da
nbqq <90015236+nbqq@users.noreply.github.com>
2021-09-03 00:42:07 -0600

    Update life.js

--- src/life.js
+++ src/life.js
@@ -99,7 +99,7 @@ class Life {
     }
 
     talentRandom() {
-        return this.#talent.talentRandom(JSON.parse(localStorage.extendTalent||'null'));
+        return this.#talent.talentRandom(localStorage.extendTalent ? (JSON.parse(localStorage.extendTalent).length > 0 ? JSON.parse(localStorage.extendTalent) : null) : null);
     }
 
     talentExtend(talentId) {
